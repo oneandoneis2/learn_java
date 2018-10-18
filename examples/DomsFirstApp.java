c2344e33783075dc01367c83252c8ce24999d1cf
Dominic Humphries <dominich@broadbean.com>
2018-10-18 17:17:35 +0100

    Minor updates

--- examples/DomsFirstApp.java
+++ examples/DomsFirstApp.java
@@ -2,5 +2,8 @@ public class DomsFirstApp {
   public static void main (String[] args) {
     System.out.println("Yay!");
     System.out.println("It works!");
+    for (int x = 0; x < 10; x++) {
+      System.out.println("x is now " + x);
+    }
   }
 }
