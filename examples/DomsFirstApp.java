9571080f137241063f53d5a7aed5d5c5c8d288bf
Dominic Humphries <dominich@broadbean.com>
2018-10-23 17:20:22 +0100

    Notes for C3, test code for handling $self equivalent

--- examples/DomsFirstApp.java
+++ examples/DomsFirstApp.java
@@ -5,5 +5,23 @@ public class DomsFirstApp {
     for (int x = 0; x < 10; x++) {
       System.out.println("x is now " + x);
     }
+
+    Dog fido = new Dog();
+    fido.name = "Fido";
+    fido.bark();
+    fido.chase("cat");
   }
+}
+
+class Dog {
+    String name;
+
+    public void bark() {
+        System.out.println(name + " goes woof!");
+    }
+
+    public void chase(String thing) {
+        bark();
+        System.out.println(name + " chases the " + thing);
+    }
 }
