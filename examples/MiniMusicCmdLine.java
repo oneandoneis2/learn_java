ffc9f1a595101a6cf45c51f708e38ac05b2d5de6
Dominic Humphries <dominich@broadbean.com>
2019-02-26 17:22:35 +0000

    Modify app to take command line arguments & change instrument from default piano

--- examples/MiniMusicCmdLine.java
+++ examples/MiniMusicCmdLine.java
@@ -2,11 +2,18 @@ import javax.sound.midi.*;
 
 public class MiniMusicCmdLine {
     public static void main (String[] args) {
-        MiniMiniMusicApp mini = new MiniMiniMusicApp();
-        mini.play();
+        MiniMusicCmdLine mini = new MiniMusicCmdLine();
+        if (args.length < 2) {
+            System.out.println("Don't forget instrument and note args!");
+        }
+        else {
+            int instrument = Integer.parseInt(args[0]);
+            int note = Integer.parseInt(args[1]);
+            mini.play(instrument, note);
+        }
     }
 
-    public void play() {
+    public void play(int instrument, int note) {
         try {
             Sequencer player = MidiSystem.getSequencer();
             player.open();
@@ -15,13 +22,18 @@ public class MiniMusicCmdLine {
 
             Track track = seq.createTrack();
 
+            ShortMessage first = new ShortMessage();
+            first.setMessage(192, 1, instrument, 0);
+            MidiEvent changeInstrument = new MidiEvent(first, 1);
+            track.add(changeInstrument);
+
             ShortMessage a = new ShortMessage();
-            a.setMessage(144, 1, 44, 100);
+            a.setMessage(144, 1, note, 100);
             MidiEvent noteOn = new MidiEvent(a, 1);
             track.add(noteOn);
 
             ShortMessage b = new ShortMessage();
-            b.setMessage(128, 1, 44, 100);
+            b.setMessage(128, 1, note, 100);
             MidiEvent noteOff = new MidiEvent(b, 16);
             track.add(noteOff);
 
