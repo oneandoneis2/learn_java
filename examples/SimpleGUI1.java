46f21b74a90cd98fc2f07e3ae704edd756090108
Dominic Humphries <dominich@broadbean.com>
2019-02-28 17:47:05 +0000

    Add event listener to button

--- examples/SimpleGUI1.java
+++ examples/SimpleGUI1.java
@@ -1,12 +1,24 @@
 import javax.swing.*;
+import java.awt.event.*;
 
-public class SimpleGUI1 {
+public class SimpleGUI1 implements ActionListener {
+
+    JButton button;
+
     public static void main (String[] args) {
+        SimpleGUI1 gui = new SimpleGUI1();
+        gui.go();
+    }
+
+    public void go () {
         // Create frame for widgets
         JFrame frame = new JFrame();
 
         // Create button widget
-        JButton button = new JButton("Click me");
+        button = new JButton("Click me");
+
+        // Add a listener to the button
+        button.addActionListener(this);
 
         // Make app terminate on close of frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -17,5 +29,9 @@ public class SimpleGUI1 {
         frame.setSize(300,300);
 
         frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent event) {
+        button.setText("I've been clicked!");
     }
 }
