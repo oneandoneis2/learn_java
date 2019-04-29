cbec5a864251df5fff8589d927469d5a32bb20e0
Dominic Humphries <dominich@broadbean.com>
2019-04-29 17:13:37 +0100

    Can run multiple threads using one runner

--- examples/MultiThread.java
+++ examples/MultiThread.java
@@ -1,4 +1,4 @@
-class MyRunnable1 implements Runnable {
+class RunThreads implements Runnable {
     public void run() {
         go();
     }
@@ -10,39 +10,13 @@ class MyRunnable1 implements Runnable {
     }
 }
 
-class MyRunnable2 implements Runnable {
-    public void run() {
-        go();
-    }
-
-    public void go() {
-        try { Thread.sleep(100); }
-        catch (Exception ex) { ex.printStackTrace(); }
-        System.out.println("two");
-    }
-}
-
-class MyRunnable3 implements Runnable {
-    public void run() {
-        go();
-    }
-
-    public void go() {
-        try { Thread.sleep(100); }
-        catch (Exception ex) { ex.printStackTrace(); }
-        System.out.println("three");
-    }
-}
-
 public class MultiThread {
     public static void main (String[] args) {
-        Runnable job1 = new MyRunnable1();
-        Runnable job2 = new MyRunnable2();
-        Runnable job3 = new MyRunnable3();
+        RunThreads runner = new RunThreads();
 
-        Thread thread1 = new Thread(job1);
-        Thread thread2 = new Thread(job2);
-        Thread thread3 = new Thread(job3);
+        Thread thread1 = new Thread(runner);
+        Thread thread2 = new Thread(runner);
+        Thread thread3 = new Thread(runner);
 
         thread1.start();
         thread2.start();
